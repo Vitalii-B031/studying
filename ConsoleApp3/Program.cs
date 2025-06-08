41a01b6b28e3950c1aa340e214d9b121cd585b0a
Vitalii-B031 <doooza3596@gmail.com>
2025-06-08 17:33:11 +0300

    Сортировка пузырьком

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int num1; 
+        Console.WriteLine("Введите размер масива:"); 
+        num1 = Convert.ToInt32(Console.ReadLine());
+        int[] array = new int[num1];
+        Random rnd = new Random();
+        for (int i = 0; i < num1; i++)
+        {
+            array[i] = rnd.Next(1, 101);
+            Console.Write(array[i]+" ");
+        }
+
+        for (int i = 0; i < num1-1; i++)
+        {
+            for (int j = 0; j < num1 - i-1; j++)
+            {
+                if (array[j] > array[j + 1])
+                {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+               
+        }
+        Console.WriteLine();
+        for (int i = 0; i < num1; i++)
+        {
+            Console.Write(array[i] + " ");
+        }
     }
 }
