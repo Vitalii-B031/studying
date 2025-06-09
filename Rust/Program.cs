05e88e513c24c23ebee09f5b7ae99ddf9efa27e2
Doooz <doooza3596@gmail.com>
2025-06-09 19:24:06 +0300

    Фабричный метод

---
+++ Rust/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Rust;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        var p = GetPlayer(GetWeapon(WeaponType.Revolver));
+        var p2 = GetPlayer(GetWeapon(WeaponType.Revolver));
+        Console.WriteLine($"{p.Name} в игре");
+        Console.WriteLine($"{p2.Name} в игре");
+        p.Hit(p2);
+        Console.WriteLine($"{p2.Name} {p2.Health}");
+        p.Print();
+    }
+
+    static Player GetPlayer(Weapon weapon)
+    {
+        var player = new Player(Console.ReadLine(), weapon);
+        return player;
+    }
+
+    static Weapon GetWeapon(WeaponType weaponType)
+    {
+        switch (weaponType)
+        {
+            case WeaponType.Bow:
+                return new Bow();
+            case WeaponType.Revolver:
+                return new Revolver();
+
+            default: throw new ArgumentException();
+        }
+    }
+}
