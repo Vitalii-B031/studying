66839249f36f512c2f953680567da95a49156c70
Doooz <doooza3596@gmail.com>
2025-06-09 18:42:51 +0300

    создание игры rust

---
+++ Rust/Player.cs
@@ -0,0 +1,25 @@
+namespace Rust;
+
+public class Player
+{
+    public string Name{get;set;}
+    public int Health{get;set;}
+    public Weapon Weapon{get;set;}
+
+    public void Hit(Player target)
+    {
+        target.Health-=Weapon.Damage;
+        Console.WriteLine($"{Name} нанес {Weapon.Damage} урона по игроку {target.Name} оружием {Weapon.Name}");
+    }
+    
+
+    public Player(string name, Weapon weapon)
+    {
+        Name=name;
+        Health=100;
+        Weapon = weapon;
+        
+
+    }
+
+}
---
+++ Rust/Weapon.cs
@@ -0,0 +1,13 @@
+namespace Rust;
+
+public class Weapon
+{
+    public string Name{get;set;}
+    public int Damage{get;set;}
+
+    public Weapon(string name, int damage)
+    {
+        Name=name;
+        Damage=damage;
+    }
+}
